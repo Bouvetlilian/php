8d1819ed1102443f43ced09d64ba7c40560a2094
Lilian <bouvetlilianpro@gmail.com>
2025-04-22 15:53:08 +0200

    modif exercice2

--- exercice2.php
+++ exercice2.php
@@ -43,7 +43,75 @@ echo "<br>";
 
 //Exercice 4 
 
+//test avec 0
 $value = 0;
 echo "Pour \$value = 0 :<br>";
 echo "is_int(\$value) : " .(is_int($value) ? "true" : "false") . "<br>";
+echo "empty(\$value) : " . (empty($value) ? "true" : "false") . "<br><br>";
+
+//test avec null
+$value = null;
+echo "Pour \$value = null :<br>";
+echo "is_int(\$value) : " .(is_int($value) ? "true" : "false") . "<br>";
+echo "empty(\$value) : " . (empty($value) ? "true" : "false") . "<br><br>";
+
+
+//test avec ""
+$value = "";
+echo "Pour \$value = \"\" (chaine vide) :<br>";
+echo "is_int(\$value) : " .(is_int($value) ? "true" : "false") . "<br>";
+echo "empty(\$value) : " . (empty($value) ? "true" : "false") . "<br><br>";
+
+
+//test avec false
+$value = false;
+echo "Pour \$value = false :<br>";
+echo "is_int(\$value) : " .(is_int($value) ? "true" : "false") . "<br>";
+echo "empty(\$value) : " . (empty($value) ? "true" : "false") . "<br><br>";
+
+
+//test avec []
+$value = [];
+echo "Pour \$value = [] (tableau vide) :<br>";
+echo "is_int(\$value) : " .(is_int($value) ? "true" : "false") . "<br>";
+echo "empty(\$value) : " . (empty($value) ? "true" : "false") . "<br><br>";
+
+
+//EXERCICE 5 
+
+$age = "35";
+
+echo "Type initial de \$age :" . gettype($age) . "<br>";
+
+settype($age, "integer");
+
+echo "Type après settype() : " . gettype($age) . "<br>";
+
+
+//EXERCICE 6 
+
+var_dump("0" == false);
+var_dump("0" === false);
+
+// == PHP convertit les types avant de comparer
+// === PHP compare les types ET les valeurs
+
+//EXERCICE 7 
+$tableau = [
+    42,
+    3.14,
+    true,
+    "bonjour",
+    null,
+    false
+];
+
+echo "contenu du tableau : <br>";
+foreach ($tableau as $index => $element) {
+    echo "Element $index : ";
+    var_dump($element);
+    echo "<br>";
+}
+
+
 ?>
