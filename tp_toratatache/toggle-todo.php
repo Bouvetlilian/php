b9aa70725f4108cda4cb6b3aed91de5a43ffcd81
Lilian <bouvetlilianpro@gmail.com>
2025-04-24 16:48:35 +0200

    ajout fichier toggle-todo

---
+++ tp_toratatache/toggle-todo.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["id"])) {
+    $id = $_POST["id"];
+
+    if (isset($_SESSION['todos'])) {
+        foreach ($_SESSION['todos'] as &$todo) {
+            if ($todo["id"] === $id) {
+                $todo["done"] = !$todo["done"];
+                break;
+            }
+        }
+    }
+
+    header("location: index.php");
+    exit();
+} else {
+    header("location: index.php");
+    exit();
+}
+
+?>
