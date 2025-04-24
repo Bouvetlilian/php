200dc33eba9a172e2459ab86a848ff75c83d0bb0
Lilian <bouvetlilianpro@gmail.com>
2025-04-24 16:54:25 +0200

    ajout du fichier todos.php

---
+++ tp_toratatache/includes/todos.php
@@ -0,0 +1,11 @@
+<?php
+
+$defaultTodos = [
+  ["id" => "a7749a0f-cf14-4b95-9864-8893c695bcaf", "name" => "apprendre le HTML", "done" => false],
+  ["id" => "28c7bc7b-c389-4962-a6bc-3be459b62fc0", "name" => "apprendre le JS", "done" => false],
+  ["id" => "d264692e-a9b9-4e30-a11f-7a7c210c7ff2", "name" => "apprendre le PHP", "done" => false],
+  ["id" => "f5454d36-53ec-473c-a083-0f309eb92b40", "name" => "coder, coder, coder", "done" => false],
+  
+];
+
+?>
